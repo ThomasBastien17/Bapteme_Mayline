213d6647cceb2e9bfaf71d31a05b5f668a408aac
thomas <thomasbastien17@gmail.com>
2025-07-21 16:29:56 +0200

    projet sur next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,75 @@
 import Image from "next/image";
+import { Corinthia } from "next/font/google";
+
+const corinthia = Corinthia({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className="flex flex-col items-center justify-start h-[100dvh]">
+      <section className="h-2/3">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/Mayline.jpg"
+          alt="Mayline"
+          width={500}
+          height={500}
+          className="shadow-lg h-full w-full object-cover object-center"
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </section>
+      <section className="flex flex-col items-center justify-center h-full w-full max-w-4xl relative overflow-visible">
+        <div className="absolute top-15 -left-75 w-full h-full pointer-events-none z-[-1]">
+          <Image
+            src="/fleurs.png"
+            alt="Fleurs"
+            fill
+            className="absolute rotate-[-10deg] scale-x-[-1] object-contain opacity-50"
+            style={{ transform: "translate(-20%, 10%)" }}
+            priority
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="flex flex-col items-center justify-center text-center h-full backdrop-blur-s bg-white/40 rounded-lg">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/croix.png"
+            alt="Croix"
+            width={100}
+            height={100}
+            className="relative -top-[33px] "
+            style={{
+              filter:
+                "invert(96%) sepia(31%) saturate(1482%) hue-rotate(291deg) brightness(95%) contrast(75%)",
+            }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <p className="text-sm">Nous célébrons le</p>
+          <h1 className={`text-9xl text-[#d6a8a7] ${corinthia.className}`}>Bapteme</h1>
+          <p className="text-sm relative left-[50px] top-[-35px] mb-5">de notre fille</p>
+          <div className="flex flex-col items-center h-full ">
+            <div className="flex flex-col items-center border-b border-[#d6a8a7] w-full">
+              <p className="flex items-center justify-center text-3xl text-[#d6a8a7] h-10  uppercase">Mayline Bastien</p>
+            </div>
+            <div className="flex flex-col items-center border-b border-[#d6a8a7] w-full">
+              <p className="flex items-center justify-center text-sm h-10">Dimanche 21 septembre 12H45</p>
+            </div>
+            <div className="flex flex-col items-center border-b border-[#d6a8a7] w-full">
+              <p className="flex items-center justify-center text-lg uppercase h-10">église de salernes</p>
+            </div>
+            <div className="flex flex-col items-center border-b border-[#d6a8a7] w-full">
+              <p className="flex items-center justify-center text-sm h-10">Puis réception à Vidauban</p>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-15 left-75 w-full h-full pointer-events-none z-[-1]">
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src="/fleurs.png"
+            alt="Fleurs"
+            fill
+            className="absolute rotate-[10deg] object-contain opacity-50"
+            style={{ transform: "translate(-20%, 10%)" }}
+            priority
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </section>
+    </main>
   );
 }
